d23aa87ecec2648a3e5441417179201374ba68fe
juanojeda <juan@juanojeda.com>
2025-07-22 14:15:13 +1000

    chore(refactor): remove unused utils

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -6,29 +6,6 @@ import React, { useEffect } from 'react';
 interface ThemeSwitcherProps extends React.PropsWithChildren {
 }
 
-type RgbaTuple = [R: number, g: number, b: number, a: number];
-const getRgbaTuple = (rgbString: string): RgbaTuple => (rgbString.replace(/^(rgb|rgba)\(/, '').replace(/\)$/, '').replace(/\s/g, '').split(',').map(val => Number(val || 1)) as RgbaTuple);
-
-const getHctFromRgbaString = (rgbString: string): Hct => {
-  
-  const [r, g, b] = getRgbaTuple(rgbString);
-
-  return Hct.from(r, g, b);
-}
-
-const getRGBAfromString = (rgbString: string): Rgba => {
-  const [r, g, b, a] = getRgbaTuple(rgbString);
-
-  return {
-    r,g,b,a
-  }
-}
-
-const rgbStringFromArgb = (argb: number): string => `rgb(${redFromArgb(argb)}, ${greenFromArgb(argb)}, ${blueFromArgb(argb)})`
-
-// Warning! Uses the private `argb` method on the Hct object
-const HctToHex = ({ hue, chroma, tone }: Hct): string => `#${Hct.from(hue, chroma, tone)['argb'].toString(16)}`;
-
 const defaultTheme = {
   primary: {
     main: "#465adb"
