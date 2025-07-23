5212f61e345f9d7a22d869d4f787e76f6ec97792
juanojeda <juan@juanojeda.com>
2025-07-23 16:30:20 +1000

    feat(audioplayer): give audioplayer skeletons

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -9,7 +9,7 @@ import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import { useYouTubePlayer } from '../hooks/useYouTubePlayer';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
+import { Box, Grid, Paper, Skeleton } from '@mui/material';
 
 interface AudioPlayerProps {
   playlist: Playlist | null;
@@ -58,6 +58,165 @@ function PlayerControlButton({ ariaLabel, icon, action }: PlayerControlButtonPro
   );
 }
 
+function AlbumArtwork({ artworkSvg }: { artworkSvg: string }) {
+  return (
+    <Paper
+      variant="elevation"
+      sx={{
+        width: 200,
+        height: 200,
+        margin: '0 auto',
+      }}
+      elevation={5}
+      dangerouslySetInnerHTML={{ __html: artworkSvg }}
+    />
+  );
+}
+
+function CurrentTrack({
+  currentTrackIndex,
+  totalTracks,
+  currentTrack,
+}: {
+  currentTrackIndex: number;
+  totalTracks: number;
+  currentTrack: { artist: string; title: string; length: number };
+}) {
+  return (
+    <Box textAlign="center" mt={2} mb={2}>
+      <Typography component={'div'} variant="subtitle2" data-testid="track-number">
+        {`Track ${currentTrackIndex + 1} of ${totalTracks}`}
+      </Typography>
+      <Typography component={'div'} variant="subtitle1" data-testid="track-title">
+        {currentTrack.title}
+      </Typography>
+      <Typography component={'div'} variant="subtitle2" data-testid="track-artist">
+        {currentTrack.artist}
+      </Typography>
+    </Box>
+  );
+}
+
+function Controls({
+  playerRef,
+  playerState,
+  playhead,
+  duration,
+}: {
+  playerRef: React.RefObject<YTPlayer | null>;
+  playerState: string | null;
+  playhead: number;
+  duration: number;
+}) {
+  return (
+    <Grid container justifyContent="center" alignItems="center" spacing={2}>
+      <PlayerControlButton
+        ariaLabel="previous"
+        icon={<SkipPreviousIcon />}
+        action={() => playerRef.current?.previousVideo && playerRef.current.previousVideo()}
+      />
+      {playerState !== 'playing' && (
+        <PlayerControlButton
+          ariaLabel="play"
+          icon={<PlayArrowIcon />}
+          action={() => playerRef.current?.playVideo && playerRef.current.playVideo()}
+        />
+      )}
+      {playerState === 'playing' && (
+        <PlayerControlButton
+          ariaLabel="pause"
+          icon={<PauseIcon />}
+          action={() => playerRef.current?.pauseVideo && playerRef.current.pauseVideo()}
+        />
+      )}
+      <PlayerControlButton
+        ariaLabel="next"
+        icon={<SkipNextIcon />}
+        action={() => playerRef.current?.nextVideo && playerRef.current.nextVideo()}
+      />
+      {
+        <Grid size={{ xs: 12 }}>
+          <Slider
+            aria-label="seek"
+            value={playhead}
+            min={0}
+            max={duration}
+            onChange={(_, value) => {
+              if (typeof value === 'number')
+                playerRef.current?.seekTo && playerRef.current.seekTo(value, true);
+            }}
+            onKeyDown={(e) => {
+              if (!playerRef.current) return;
+              if (e.key === 'ArrowRight') {
+                handleSeekArrowKey(playerRef.current, 1);
+              } else if (e.key === 'ArrowLeft') {
+                handleSeekArrowKey(playerRef.current, -1);
+              }
+            }}
+          />
+          <Grid container justifyContent="space-between">
+            <Typography variant="caption" data-testid="track-playhead">
+              {formatTime(playhead)}
+            </Typography>
+            <Typography variant="caption" data-testid="track-length">
+              {formatTime(duration)}
+            </Typography>
+          </Grid>
+        </Grid>
+      }
+    </Grid>
+  );
+}
+
+function TrackDataSkeleton() {
+  return (
+    <Box mt={2} mb={1} display={'flex'} flexDirection="column" alignItems="center">
+      <Skeleton variant="text" width={200} />
+      <Skeleton variant="text" width={200} />
+    </Box>
+  );
+}
+
+function ControlsSkeleton() {
+  return (
+    <Box display={'flex'} flexDirection="column" alignItems="center">
+      <Skeleton
+        variant="rectangular"
+        width={200}
+        height={40}
+        sx={{ marginBottom: 3, marginTop: 1.5 }}
+      />
+      <Skeleton
+        variant="rectangular"
+        width={'100%'}
+        height={35}
+        sx={{
+          marginBottom: '.8rem',
+        }}
+      />
+    </Box>
+  );
+}
+
+function PlayerSkeleton() {
+  return (
+    <Grid container direction={'column'} justifyContent="center" alignItems="center" spacing={2}>
+      <Grid size={{ xs: 12 }} textAlign="center">
+        <Skeleton
+          variant="rectangular"
+          width={200}
+          height={200}
+          sx={{
+            margin: '0 auto',
+          }}
+        />
+        <TrackDataSkeleton />
+        <ControlsSkeleton />
+      </Grid>
+    </Grid>
+  );
+}
+
 const AudioPlayer: React.FC<AudioPlayerProps> = ({ playlist }) => {
   const {
     playerRef,
@@ -71,82 +230,36 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ playlist }) => {
     duration,
   } = useYouTubePlayer(playlist);
 
-  if (!playlist) return null;
+  if (!playlist) return <PlayerSkeleton />;
   return (
-    <div>
+    <Grid justifyContent={'center'} container>
       <div id={playerDivId.current} />
-      {playerReady && (
-        <>
-          <Typography variant="subtitle2" data-testid="track-number">
-            {`Track ${currentTrackIndex + 1} of ${totalTracks}`}
-          </Typography>
-          <Typography variant="subtitle1" data-testid="track-title">
-            {currentTrack.title}
-          </Typography>
-          <Typography variant="subtitle2" data-testid="track-artist">
-            {currentTrack.artist}
-          </Typography>
-          <PlayerControlButton
-            ariaLabel="prev"
-            icon={<SkipPreviousIcon />}
-            action={() => playerRef.current?.previousVideo && playerRef.current.previousVideo()}
-          />
-          {playerState !== 'playing' && (
-            <PlayerControlButton
-              ariaLabel="play"
-              icon={<PlayArrowIcon />}
-              action={() => playerRef.current?.playVideo && playerRef.current.playVideo()}
-            />
+      {
+        <Grid alignContent={'center'} alignSelf={'center'} justifyContent={'center'} size={12}>
+          <AlbumArtwork artworkSvg={playlist.artworkSvg} />
+          {playerReady ? (
+            <>
+              <CurrentTrack
+                currentTrackIndex={currentTrackIndex}
+                totalTracks={totalTracks}
+                currentTrack={currentTrack}
+              />
+              <Controls
+                playerRef={playerRef}
+                playerState={playerState}
+                playhead={playhead}
+                duration={duration}
+              />
+            </>
+          ) : (
+            <>
+              <TrackDataSkeleton />
+              <ControlsSkeleton />
+            </>
           )}
-          {playerState === 'playing' && (
-            <PlayerControlButton
-              ariaLabel="pause"
-              icon={<PauseIcon />}
-              action={() => playerRef.current?.pauseVideo && playerRef.current.pauseVideo()}
-            />
-          )}
-          <PlayerControlButton
-            ariaLabel="next"
-            icon={<SkipNextIcon />}
-            action={() => playerRef.current?.nextVideo && playerRef.current.nextVideo()}
-          />
-          {duration > 0 && (
-            <Grid container justifyContent={'space-between'}>
-              <Grid size={12}>
-                <Slider
-                  aria-label="seek"
-                  value={playhead}
-                  min={0}
-                  max={duration}
-                  onChange={(_, value) => {
-                    if (typeof value === 'number')
-                      playerRef.current?.seekTo && playerRef.current.seekTo(value, true);
-                  }}
-                  onKeyDown={(e) => {
-                    if (!playerRef.current) return;
-                    if (e.key === 'ArrowRight') {
-                      handleSeekArrowKey(playerRef.current, 1);
-                    } else if (e.key === 'ArrowLeft') {
-                      handleSeekArrowKey(playerRef.current, -1);
-                    }
-                  }}
-                />
-              </Grid>
-              <Grid>
-                <Typography variant="caption" data-testid="track-playhead">
-                  {formatTime(playhead)}
-                </Typography>
-              </Grid>
-              <Grid>
-                <Typography variant="caption" data-testid="track-length">
-                  {formatTime(duration)}
-                </Typography>
-              </Grid>
-            </Grid>
-          )}
-        </>
-      )}
-    </div>
+        </Grid>
+      }
+    </Grid>
   );
 };
 
